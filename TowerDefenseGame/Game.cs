efb2e7e761175d69025a1ee6bac7a6ffc20e7c51
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-05-03 21:07:10 -0500

    Added encapsulation and arrays

--- TowerDefenseGame/Game.cs
+++ TowerDefenseGame/Game.cs
@@ -11,7 +11,23 @@ namespace TowerDefense
             Map map = new Map(8, 5);
 
             try {
-                MapLocation mapLocation = new MapLocation(20, 20, map);
+                Path path = new Path(
+                    new [] {
+                        new MapLocation(0, 2, map),
+                        new MapLocation(1, 2, map),
+                        new MapLocation(2, 2, map),
+                        new MapLocation(3, 2, map),
+                        new MapLocation(4, 2, map),
+                        new MapLocation(5, 2, map),
+                        new MapLocation(6, 2, map),
+                        new MapLocation(7, 2, map)
+                    }
+                );
+
+                MapLocation location = path.GetLocationAt(0);
+                if (location != null) {
+                    Console.WriteLine(location.X + "," + location.Y);
+                }
             }
             catch (OutOfBoundsException ex) {
                 Console.WriteLine(ex.Message);
@@ -19,8 +35,8 @@ namespace TowerDefense
             catch (DefenseException) {
                 Console.WriteLine("Unhandled tree house defense exception");
             }
-            catch (Exception) {
-                Console.WriteLine("Unhandled exception");
+            catch (Exception ex) {
+                Console.WriteLine("Unhandled exception: " + ex);
             }
 
             Console.ReadLine();
--- TowerDefenseGame/Path.cs
+++ TowerDefenseGame/Path.cs
@@ -1,6 +1,14 @@
 namespace TowerDefense
 {
     class Path {
-        
+        private readonly MapLocation[] path;
+
+        public Path(MapLocation[] path) {
+            this.path = path;
+        }
+
+        public MapLocation GetLocationAt(int pathStep) {
+            return (pathStep < this.path.Length) ? this.path[pathStep] : null;
+        }
     }
 }
